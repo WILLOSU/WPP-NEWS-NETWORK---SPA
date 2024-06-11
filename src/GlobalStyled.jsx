43af45bb9358a83a011b8c4b6a468530d837363c
WILLOSU <WILLOSU>
2024-06-11 09:17:38 -0300

    x

--- src/GlobalStyled.jsx
+++ src/GlobalStyled.jsx
@@ -1,7 +1,8 @@
 import { createGlobalStyle } from "styled-components";
+import { Helmet } from 'react-helmet';
 
 export const GlobalStyled = createGlobalStyle`
-<<<<<<< HEAD
+
   * {
     margin: 0;
     padding: 0;
@@ -13,7 +14,7 @@ export const GlobalStyled = createGlobalStyle`
     text-decoration: none;
   }
 
-=======
+
 
 @import url('https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200;0,6..72,300;0,6..72,400;0,6..72,500;0,6..72,600;0,6..72,700;0,6..72,800;1,6..72,200;1,6..72,300;1,6..72,400;1,6..72,500;1,6..72,600;1,6..72,700;1,6..72,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 
@@ -23,7 +24,7 @@ export const GlobalStyled = createGlobalStyle`
     padding: 0;
     font-family: Newsreader, Arial;
     }
->>>>>>> 7d7b71804870049f702d79b1c9ff90b6e6b10547
+
   html {
     width: auto;
   }
