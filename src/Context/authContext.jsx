51941e33d7fd69fa92403bba153ceb302889ee62
WILLOSU <WILLOSU>
2024-06-11 14:06:06 -0300

    doc

--- src/Context/authContext.jsx
+++ src/Context/authContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getUserService } from "../Services/userServices";
 
-const routeAuth = "https://willnews-api.onrender.com/doc/";
+const routeAuth = "https://wpp-news-network-api.onrender.com";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
